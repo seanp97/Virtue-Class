ec858474d1ac1de743a5bca7dab2b5d6798131d7
seanp97 <sean.pelser97@gmail.com>
2020-06-16 22:16:51 +0100

    Update Virtue.js

--- Virtue.js
+++ Virtue.js
@@ -85,7 +85,7 @@ class Virtue {
         for(let j = 0; j < x.length; j++) {
             for (let i = 0; i < x.length; i++) {
                 if (x[i] > x[i + 1]) {
-                    var swap = x[i];
+                    let swap = x[i];
                     x[i] = x[i + 1];
                     x[i + 1] = swap;
                 }
